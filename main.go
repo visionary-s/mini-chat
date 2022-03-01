24bc0efee3e1413b5bad669e56703b2595acccf8
Orianna_S <shibiyun2013@163.com>
2022-03-01 18:35:06 +0800

    fix main

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"mini-chat/models"
 	"mini-chat/routers"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
-func init() {
+func Init() {
 	viper.SetConfigType("json")
+
+	// init log
+	gin.DisableConsoleColor()
+	logfile := "/logs/gin.log"
+	f, _ := os.Create(logfile)
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// init DB
 	models.New().InitDB()
 }
 
+func StartRoute() {
+	router := routers.Router()
+	html := template.Must(template.ParseFiles("/view/index.html"))
+	router.SetHTMLTemplate(html)
+}
+
 func main() {
-	router := gin.Default()
-	routers.Router()
+	go Init()
+	go StartRoute()
 }
--- models/db.go
+++ models/db.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,7 +16,7 @@ func New() *connection {
 
 // https://gorm.io/docs/connecting_to_the_database.html
 func (c *connection) InitDB() *gorm.DB {
-	dsn := viper.GetString(`mysql.dsn`)
+	dsn := "root:root@tcp(127.0.0.1:3306)/mini_chat?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Failed to initialize DB!!!")
